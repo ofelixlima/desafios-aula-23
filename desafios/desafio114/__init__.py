71d1e02335495723ae0d9ed8d37d8d6d2fcd2250
Felix Lima <32372236+ofelixlima@users.noreply.github.com>
2026-01-29 12:10:25 -0300

    desafio 114 resolvido

---
+++ desafios/desafio114/__init__.py
@@ -0,0 +1,10 @@
+import requests
+from time import sleep
+try:
+    resposta = requests.get("https://pudim.com.br")
+    if resposta.status_code == 200:
+        print(f"\033[1;32mSite do pudim está ativo!\033[m")
+except requests.exceptions.ConnectionError:
+    print(f"\033[1;31mErro! Não consigo acessar o site do pudim por erro de conexão.\033[m")
+else:
+    print(f'\033[1;32mConexão bem-sucedida!\033[m')
