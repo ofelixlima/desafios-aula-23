ed54db2c9a967ffc729fc876ef72aaa6c50a7c91
Felix Lima <32372236+ofelixlima@users.noreply.github.com>
2026-01-30 12:35:40 -0300

    Aprimorando a interface do sistema

--- desafios/desafio115/sistema.py
+++ desafios/desafio115/sistema.py
@@ -1,4 +1,14 @@
 from lib.verificacao import encontreArquivo
-from lib.interface import cabecalho, menu
-menu(['opção 01', 'opção 02', 'opção 03'])
+from lib.interface import cabecalho, menu, submenu
+
+while True:    
+    lista = ["Ver pessoas cadastradas", "Cadastrar nova pessoa", "Sair do Sistema"]           
+    m = menu(lista)
+    if m == 0:
+        cabecalho('Opção 1')
+    elif m == 1:
+        cabecalho('Opção 2')
+    elif m == 2:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
 #encontreArquivo("desafios/desafio115/dist/verificacao/lista.txt")
