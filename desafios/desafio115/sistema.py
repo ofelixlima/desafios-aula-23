c22f1dde0e3e803a90d60f2a61bd6182d8d487f2
Felix Lima <32372236+ofelixlima@users.noreply.github.com>
2026-01-31 12:42:25 -0300

    Arquivo principal e arquivo de backup atualizados simultaneamente - Desafio 115 completo

--- desafios/desafio115/sistema.py
+++ desafios/desafio115/sistema.py
@@ -2,29 +2,25 @@ from lib.verificacao import *
 from lib.interface import *
 from time import sleep
 from lib.leitura import *
+
 while True:    
     lista = ["Ver pessoas cadastradas", "Cadastrar nova pessoa", "Sair do Sistema"]           
     m = menu(lista)
     caminho = "desafios/desafio115/lib/verificacao/lista.txt"
     back = 'desafios/desafio115/lib/backup/backup.txt'
     if m == 0:
-        if arquivoExiste(back):
-            if not arquivoExiste(caminho):
-                criarArquivo(caminho)
-                escreveArquivo(caminho, leituraCompleta(back))
-        elif arquivoExiste(caminho):
-            if not arquivoExiste(back):
-                criarArquivo(back)
-                salveBackup(back, caminho)
+        doBackupParaArquivo(back, caminho)
         cabecalho('PESSOAS CADASTRADAS')
         pessoas = leituraLinha(caminho)
         organizarLista(pessoas)
     elif m == 1:
+        doBackupParaArquivo(back, caminho)
         cabecalho('NOVO CADASTRO')
         pessoa = cadastroNome().strip()
         idade = leiaInt('Idade: ')
         todos = f'{pessoa};{idade}'
         atualizaArquivo(caminho, todos)
+        atualizaArquivo(back, todos)
         print(f"{nomePessoa(pessoa)} com a idade {idade} foi adicionado com sucesso!")
     elif m == 2:
         cabecalho('Saindo do sistema... Até logo!')
